77d7fb3fbc3aaa6267a29066488a9a7427b8da92
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-05-18 23:53:22 -0400

    webpack hot-reloading setup

--- src/components/account/ProfilePage.jsx
+++ src/components/account/ProfilePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default function HomePage() {
+export default function ProfilePage(props) {
+  const { match } = props;
   return (
     <section className="page-content">
       <div className="content-container">
-        This is the profile page.
+        This is the profile page The profile id is: {match.params.id}.
       </div>
       <aside className="sidebar">
         This is a sidebar
